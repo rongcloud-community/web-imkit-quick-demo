415343276e046ca35c6f5e9fe98859d936086c80
祝星耀 <zhuxingyao1@rongcloud.cn>
2024-12-26 10:41:04 +0800

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Navbar from './Navbar.vue';
+
+</script>
+
+<template>
+  <div class="app-layout">
+    <Navbar></Navbar>
+    <div class="app-layout-conten">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app-layout {
+  display: flex;
+  flex: 1;
+  overflow: hidden;
+  
+  .app-layout-conten {
+    flex: 1;
+    display: flex;
+  }
+}
+</style>
